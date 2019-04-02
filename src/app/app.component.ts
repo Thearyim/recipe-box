323c0a18fa0467701db2911c5b9e41e02bd27b46
Guest User <Guest@epicodus-31.local>
2019-04-02 15:49:27 -0700

    Add constructor and button to display recipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Dinner Recipe';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  recipes: Recipe[] = [
+    new Recipe('Chow Mein'),
+    new Recipe('Chicken Piccata'),
+    new Recipe('Beef Stew'),
+    new Recipe('Garlic Brown Sugar Glazed Salmon')
+  ];
+
+  selectedRecipe: null;
+
+    viewRecipe(clickedRecipe) {
+      this.selectedRecipe = clickedRecipe;
+    }
+
+    hideRecipe() {
+      this.selectedRecipe = null;
+    }
+
 }
---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,4 @@
+export class Recipe {
+  public viewed: boolean = false;
+  constructor(public description: string) { }
+}
